8b53e8eb05c2994d3e42792e9da2120b05e31bb7
Yuriy Naydenov <ebceu44@gmail.com>
2019-06-28 14:13:49 +0300

    npm fix

--- src/extensions.ts
+++ src/extensions.ts
@@ -1,1 +1,143 @@
-/Users/ebceu4/projects/node/waves-hands/src/app/hands/extensions.ts
+interface Array<T> {
+  toRecord(map?: (item: T) => string): Record<string, T>
+  removeLast(): Array<T>
+  removeFirst(): Array<T>
+  firstOrUndefined(): T | undefined
+  firstOrDefault(d: T): T
+  first(): T
+  lastOrUndefined(): T | undefined
+  lastOrDefault(d: T): T
+  last(): T
+  tryMapOrUndefined<TOut>(map: (item: T) => TOut): (TOut | undefined)[]
+  min(map: (item: T) => number): T
+  max(map: (item: T) => number): T
+  sum(map: (item: T) => number): number
+  minMax(map: (item: T) => number): { min: T, max: T }
+  minMaxIndex(map: (item: T) => number): { min: number, max: number }
+  minIndex(map: (item: T) => number): number
+  maxIndex(map: (item: T) => number): number
+  any(predicate: (item: T) => boolean): boolean
+  orderBy(orderProp: (item: T) => keyof T): Array<T>
+}
+
+Array.prototype.tryMapOrUndefined = function <T, TOut>(map: (item: T) => TOut) {
+  return this.map(x => {
+    try {
+      return map(x)
+    } catch (error) {
+      return undefined
+    }
+  })
+}
+
+Array.prototype.any = function <T>(predicate: (item: T) => boolean) {
+  for (let i = 0; i < this.length; i++) {
+    if (predicate(this[i]))
+      return true
+  }
+  return false
+}
+
+Array.prototype.toRecord = function <T>(map?: (item: T) => string) {
+  return this.reduce((a, b) => ({ ...a, [map!(b)]: b }), {})
+}
+
+Array.prototype.firstOrUndefined = function () {
+  return this.length > 0 ? this[0] : undefined
+}
+
+Array.prototype.firstOrDefault = function <T>(d: T) {
+  return this.length > 0 ? this[0] : d
+}
+
+Array.prototype.first = function () {
+  if (this.length > 0)
+    return this[0]
+
+  throw new Error('Array is empty.')
+}
+
+Array.prototype.removeFirst = function () {
+  if (this.length > 0)
+    return this.splice(1)
+
+  throw new Error('Array is empty.')
+}
+
+Array.prototype.removeLast = function () {
+  if (this.length > 0)
+    return this.splice(0, this.length - 1)
+
+  throw new Error('Array is empty.')
+}
+
+Array.prototype.lastOrUndefined = function () {
+  return this.length > 0 ? this[this.length - 1] : undefined
+}
+
+Array.prototype.lastOrDefault = function <T>(d: T) {
+  return this.length > 0 ? this[this.length - 1] : d
+}
+
+Array.prototype.last = function () {
+  if (this.length > 0)
+    return this[this.length - 1]
+
+  throw new Error('Array is empty.')
+}
+
+Array.prototype.sum = function <T>(map: (item: T) => number) {
+  return this.map(map).reduce((a, b) => a + b, 0)
+}
+
+Array.prototype.minIndex = function <T>(map: (item: T) => number) {
+  return this.minMaxIndex(map).min
+}
+
+Array.prototype.maxIndex = function <T>(map: (item: T) => number) {
+  return this.minMaxIndex(map).max
+}
+
+Array.prototype.min = function <T>(map: (item: T) => number) {
+  return this[this.minIndex(map)]
+}
+
+Array.prototype.minMaxIndex = function <T>(map: (item: T) => number) {
+  let min
+  let max
+  let minIndex = 0
+  let maxIndex = 0
+  for (let i = 0; i < this.length; i++) {
+    const element = this[i]
+    const value = map(element)
+    min = Math.min(value, min || value)
+    max = Math.max(value, max || value)
+    if (max === value) {
+      maxIndex = i
+    }
+    if (min === value) {
+      minIndex = i
+    }
+  }
+  return { min: minIndex, max: maxIndex }
+}
+
+Array.prototype.max = function <T>(map: (item: T) => number) {
+  return this[this.maxIndex(map)]
+}
+
+Array.prototype.minMax = function <T>(map: (item: T) => number) {
+  const { min, max } = this.minMaxIndex(map)
+  return { min: this[min], max: this[max] }
+}
+
+Array.prototype.orderBy = function <T>(orderProp: (item: T) => keyof T): Array<T> {
+  const tmp = new Array<T>(...this)
+  return tmp.sort((a, b) => {
+    const aProp = orderProp(a)
+    const bProp = orderProp(b)
+    return aProp > bProp ? 1 : (aProp < bProp ? -1 : 0)
+  })
+}
+
+
--- src/prompts-generic.ts
+++ src/prompts-generic.ts
@@ -1,1 +1,132 @@
-/Users/ebceu4/projects/node/liga-scripts/src/prompts-generic.ts
+
+import { readFileSync } from 'fs'
+import { resolve } from 'path'
+import './extensions'
+import { green, end } from './colors'
+import prompt from 'prompts'
+
+
+export const promptOneOf = async <T>(options: { value: T, title: string }[], text: string): Promise<T> => {
+  const result = await prompt({
+    type: 'select',
+    name: 'value',
+    message: text,
+    choices: options.map(({ value, title }) => ({ value: JSON.stringify(value), title })),
+  })
+
+  if (!result.value)
+    throw new Error('Aborted by user.')
+
+  return JSON.parse(result.value) as T
+}
+
+
+export const promptForFile = async (text: string, contentValidator?: (content: string) => boolean | string): Promise<string> => {
+  const result = await prompt({
+    type: 'text',
+    name: 'value',
+    message: text,
+
+    validate: filePath => {
+      try {
+        const content = readFileSync(resolve(process.cwd(), filePath), { encoding: 'utf8' })
+        return contentValidator ? contentValidator(content) : true
+      } catch (error) {
+        return error.message
+      }
+    },
+  })
+
+  if (!result.value)
+    throw new Error('Aborted by user.')
+
+  return readFileSync(resolve(process.cwd(), result.value), { encoding: 'utf8' })
+}
+
+export const promptForNumber = async (text: string, validate?: (value: number) => boolean | string): Promise<number> => {
+  const result = await prompt({
+    type: 'number',
+    name: 'value',
+    message: text,
+    validate,
+  })
+
+  if (!result.value)
+    throw new Error('Aborted by user.')
+
+  return result.value
+}
+
+export const promptForDate = async (text: string, validate?: (date: Date) => boolean | string): Promise<Date> => {
+  const result = await prompt({
+    type: 'date',
+    name: 'value',
+    message: text,
+  })
+
+  if (!result.value)
+    throw new Error('Aborted by user.')
+
+  return result.value
+}
+
+export const promptForString = async (text: string, options?: { regexp: RegExp, errorMessage: string }): Promise<string> => {
+  const result = await prompt({
+    type: 'text',
+    name: 'value',
+    message: text,
+    validate: answer => {
+      if (!answer) {
+        return 'Provide a non empty value please.'
+      }
+
+      if (options) {
+        if (!options.regexp.test(answer)) {
+          return options.errorMessage
+        }
+      }
+
+      return true
+    },
+  })
+
+  if (!result.value)
+    throw new Error('Aborted by user.')
+
+  return result.value
+}
+
+export const promptConfirmation = async (text: string): Promise<boolean> => {
+  const result = await prompt({
+    type: 'toggle',
+    name: 'value',
+    message: text,
+    initial: false,
+    active: 'YES',
+    inactive: 'NO',
+  })
+
+  if (result.value == undefined)
+    throw new Error('Aborted by user.')
+
+  return result.value
+}
+
+export const spinner = (text: string, done?: string) => {
+  console.log('')
+  var spinner = require('cli-spinner').Spinner
+  var spinner = new spinner(text)
+  spinner.setSpinnerString('⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏')
+  spinner.start()
+
+  return {
+    stop: () => {
+      spinner.stop(true)
+      if (done) {
+        console.log(done)
+      } else {
+        console.log(text, `${green}✔${end}`)
+      }
+    },
+  }
+}
