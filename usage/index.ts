2c7207e0757468368a0488911ff40358c6bf0363
Yuriy Naydenov <ebceu44@gmail.com>
2019-07-01 18:24:05 +0300

    version update

--- usage/index.ts
+++ usage/index.ts
@@ -1,36 +1,30 @@
-import { Items } from '../src/index'
-import { ChainId } from '../src/types'
+import { wavesItems } from '../src/index'
+import { address } from '@waves/waves-crypto'
 
 const seed = '9e0fa09dcc6e40ae95f5d8e6795c76c5fb97507be9ff40d9b6f58df27c171e53'
 
 async function createItem() {
-  const items = Items(ChainId.Testnet)
-  const request = items.create({ amount: 100, version: 1, name: 'The sword of pain', imageUrl: 'img_url', misc: {}, isLimited: true }, seed)
-  const item = await request.execute()
+  const items = wavesItems('T')
+  const request = items.createItem({ quantity: 100, version: 1, name: 'The sword of pain', imageUrl: 'img_url', misc: {} })
+  const item = await request.broadcast(seed)
   console.log(item)
 }
 createItem()
 
 
 async function getItem() {
-  const items = Items(ChainId.Testnet)
-  const item = await items.getItem('6vq5YokeJUeCU3BCah65LTgiS5ESzjJdNurddY4ZDhcG', false)
+  const items = wavesItems('T')
+  const item = await items.getItem('6vq5YokeJUeCU3BCah65LTgiS5ESzjJdNurddY4ZDhcG')
   console.log(item)
 }
 //getItem()
 
 
 async function getItemList() {
-  const items = Items(ChainId.Testnet)
-  const list = await items.getItemList('creatorAddress')
+  const items = wavesItems('T')
+  const list = await items.getItemCatalog(address(seed, 'T'))
   console.log(list)
 }
 //getItemList()
 
 
-//If item was not previously freezeed you can change amount of items created
-async function changeAmount() {
-  const items = Items(ChainId.Testnet)
-  items.changeAmount('6vq5YokeJUeCU3BCah65LTgiS5ESzjJdNurddY4ZDhcG', 10, false, seed)
-}
-//changeAmount()
