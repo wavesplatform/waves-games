6c394e3cd548f1c8d5038bf2e8c6ca172667553a
Yuriy Naydenov <ebceu44@gmail.com>
2019-07-03 18:20:47 +0300

    *

--- test/index.ts
+++ test/index.ts
@@ -1,4 +1,4 @@
-import { wavesItemsApi } from '../src/index'
+import { wavesItemsApi, parseDataPayload } from '../src/index'
 const seed = '9e0fa09dcc6e40ae95f5d8e6795c76c5fb97507be9ff40d9b6f58df27c171e53'
 
 async function createItem() {
@@ -23,9 +23,20 @@ async function getItemList() {
 }
 //getItemList()
 
-async function getUserItems() {
+async function getUserInventory() {
   const items = wavesItemsApi('W')
-  const list = await items.getUserInventory('3PKEQiRe2u6488jdvUAUYshrM4fQPf4omak', '3PKEQiRe2u6488jdvUAUYshrM4fQPf4omak')
+  const list = await items.getUserInventory(
+    '3PKEQiRe2u6488jdvUAUYshrM4fQPf4omak',
+    '3PKEQiRe2u6488jdvUAUYshrM4fQPf4omak',
+  )
   console.log(list)
 }
-getUserItems()
+//getUserInventory()
+
+async function parsePayload() {
+  const { version, data } = parseDataPayload(
+    '{"version":1,"name":"Shadow Era: Call of the Crystals - Cobra Demon (se056)","imageUrl":"https://cdn.shadowera.com/cards/shdw/se056.jpg","misc":{"set":"Call of the Crystals","number":56,"id":"se056"}}',
+  )
+  console.log(version, data)
+}
+parsePayload()
